83f4aa6518c681488b97f854b0db3ff0266f5fd2
Chris Buffalino <chris@cbeyondmgt.com>
2017-11-09 12:06:36 -0800

    Linked JS File
    Moved my JS code from inline to its own file and directory inside the
    root folder.

---
+++ scripts/hw1.js
@@ -0,0 +1,30 @@
+//Challenge 1//
+var a = 100;
+var b = 200;
+document.getElementById("c1").innerHTML = a + b;
+console.log(a + b);
+    
+//Challenge 2//
+var num = 7;
+if (num > 5) {
+    document.getElementById("c2").innerHTML = num * 3;
+    console.log(num * 3);
+    
+} else {
+    document.getElementById("c2").innerHTML = "enter a bigger number :)";
+    console.log("enter a bigger number :)");
+}
+ //Challenge 3//   
+var num2 = 19;
+if (num2 % 2 === 0) {
+    document.getElementById("c3").innerHTML = num2 + " is even";
+    console.log(num2 + " is even");
+} else {
+    document.getElementById("c3").innerHTML = num2 + " is odd";
+    console.log(num2 + " is odd");
+}
+//Challenge 4//    
+var num3 = 600;
+document.getElementById("c4").innerHTML = "The meaning of life is " + num3;
+console.log("The meaning of life is " + num3);
+
